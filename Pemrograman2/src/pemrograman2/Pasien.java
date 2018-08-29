c1a1aa2e378e91149943eea59183d569ffee1170
admin <admin@9b>
2018-08-29 11:46:59 +0700

    versi 1

---
+++ Pemrograman2/src/pemrograman2/Pasien.java
@@ -0,0 +1,17 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package pemrograman2;
+
+/**
+ *
+ * @author admin
+ */
+public class Pasien {
+private String Nama;
+private String Alamat;
+private String TempatLahir;
+private String noRekamMedis;
+}
