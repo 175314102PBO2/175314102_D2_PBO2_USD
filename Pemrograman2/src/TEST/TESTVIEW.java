2ba886a493567d92e03a73f2e6bbe80e75e972f7
admin <admin@15b>
2018-09-05 11:56:09 +0700

    TEST

---
+++ Pemrograman2/src/TEST/TESTVIEW.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package TEST;
+
+import VIEW.DaftarAntrianDialog;
+
+/**
+ *
+ * @author admin
+ */
+public class TESTVIEW {
+    public static void main(String[] args) {
+        DaftarAntrianDialog test = new DaftarAntrianDialog();
+        test.setSize(500, 500);
+        test.setVisible(true);
+    }
+}
+
