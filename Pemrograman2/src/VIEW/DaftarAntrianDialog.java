9f93eb5f04bd6f1473bbdfe11a3972e2c777b5f3
admin <admin@15b>
2018-09-05 11:55:28 +0700

    1.2

---
+++ Pemrograman2/src/VIEW/DaftarAntrianDialog.java
@@ -0,0 +1,65 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package VIEW;
+
+import javax.swing.JButton;
+import javax.swing.JDialog;
+import javax.swing.JLabel;
+import javax.swing.JTextField;
+
+/**
+ *
+ * @author admin
+ */
+public class DaftarAntrianDialog extends JDialog{
+    private JLabel NamaLabel;
+    private JLabel JudulLabel;
+    private JLabel AlamatLabel;
+    private JLabel NoRekamMedisLabel;
+    private JTextField NoRekamMedisText;
+    private JTextField JudulText;
+    private JTextField NamaText;
+    private JButton SaveButton;
+
+    public DaftarAntrianDialog() {
+        init();
+    }
+    
+    public void init() {
+        this.setLayout(null);
+        JudulLabel = new JLabel (" DAFTAR NAMA PASIEN ");
+        JudulLabel.setBounds(98, 10, 200, 10); 
+        this.add(JudulLabel);
+        
+        NamaLabel = new JLabel(" NAMA ");
+        NamaLabel.setBounds(10, 40, 50, 25);
+        this.add(NamaLabel);
+        
+        NamaText = new JTextField();
+        NamaText.setBounds(70, 40, 50, 25);
+        this.add(NamaText);
+        
+        
+        NoRekamMedisLabel = new JLabel (" NOMOR REKAM MEDIS   ");
+        NoRekamMedisLabel.setBounds(10, 70, 300, 25);
+        this.add(NoRekamMedisLabel);
+        
+        NoRekamMedisText = new JTextField();
+        NoRekamMedisText.setBounds(100, 70, 300, 25);
+        this.add(NoRekamMedisText);
+        
+        AlamatLabel = new JLabel (" ALAMAT ");;
+        AlamatLabel.setBounds(10, 90, 60, 25);
+        this.add(AlamatLabel);
+        
+        SaveButton = new JButton(" SIMPAN ");
+        SaveButton.setBounds(60, 350, 90, 30);
+        this.add(SaveButton);
+        
+    }
+}
+    
+
