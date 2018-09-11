99b92730e4d3be06871869fcaeeaf5626dac2ced
ACER <ACER@LAPTOP-7IS31PT0>
2018-09-11 08:34:39 +0700

    1.1

---
+++ Pemrograman2/src/TEST/TESTVIEW_JFRAME.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package TEST;
+
+import javax.swing.JFrame;
+
+/**
+ *
+ * @author ACER
+ */
+public class TESTVIEW_JFRAME {
+    public static void main(String[] args) {
+        JFrame test = new JFrame();
+        test.setSize(500, 500);
+        test.setVisible(true);
+    }
+   
+}
