4836355aa94cc83c4bb7bed1933c42d6fb95dbc7
ACER <ACER@LAPTOP-7IS31PT0>
2018-09-19 10:32:03 +0700

    hmm

---
+++ Pemrograman2/src/VIEW/FrameUtama.java
@@ -0,0 +1,23 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package VIEW;
+
+import javax.swing.JFrame;
+import javax.swing.JMenu;
+import javax.swing.JMenuBar;
+import javax.swing.JMenuItem;
+
+/**
+ *
+ * @author admin
+ */
+public class FrameUtama extends JFrame {
+    private JMenuBar menuBar;
+    private JMenu pasienMenu;
+    private JMenuItem tambahPasienMenuItem;
+    
+    
+}
