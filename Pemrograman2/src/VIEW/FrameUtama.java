7e5fec121eb5893e800006ffc0a41d6c50cff690
ACER <ACER@LAPTOP-7IS31PT0>
2018-09-24 19:14:53 +0700

    hmm

--- Pemrograman2/src/VIEW/FrameUtama.java
+++ Pemrograman2/src/VIEW/FrameUtama.java
@@ -5,6 +5,8 @@
  */
 package VIEW;
 
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 import javax.swing.JFrame;
 import javax.swing.JMenu;
 import javax.swing.JMenuBar;
@@ -14,10 +16,64 @@ import javax.swing.JMenuItem;
  *
  * @author admin
  */
-public class FrameUtama extends JFrame {
-    private JMenuBar menuBar;
-    private JMenu pasienMenu;
-    private JMenuItem tambahPasienMenuItem;
+public class FrameUtama extends JFrame implements ActionListener {
+
+    private JMenuBar MenuBar;
+    private JMenu fileMenu;
+    private JMenuItem exitMenuItem;
+    private JMenuItem TambahPasien;
+    private JMenuItem TambahAntrian;
+
+    public FrameUtama() {
+        init();
+    }
+
+    public void init() {
+        //membuat menuBar dengan nama Pasien
+        MenuBar = new JMenuBar();
+        fileMenu = new JMenu("Pasien");
+
+        MenuBar.add(fileMenu);
+        this.setJMenuBar(MenuBar);
+        //menambah MenuBar dengan ini TambahPasien, TambahAntrian, exitMenuItem
+        exitMenuItem = new JMenuItem("Exit");
+        TambahPasien = new JMenuItem("Tambah Pasien Baru");
+        TambahAntrian = new JMenuItem("Tambah Antrian");
+
+        fileMenu.add(TambahPasien);
+        fileMenu.add(TambahAntrian);
+        fileMenu.add(exitMenuItem);
+        MenuBar.add(fileMenu);
+
+        exitMenuItem.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                System.exit(0);
+            }
+        });
+        TambahPasien.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                TambahPasienBaruDialog tampil = new TambahPasienBaruDialog();
+                tampil.setSize(400, 500);
+                tampil.setVisible(true);
+            }
+        });
+        TambahAntrian.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                TambahAntrianDialog tampil = new TambahAntrianDialog();
+                tampil.setSize(400, 500);
+                tampil.setVisible(true);
+            }
+        });
+
+    }
+
+    @Override
+    public void actionPerformed(ActionEvent e) {
+        
+    }
     
     
 }
