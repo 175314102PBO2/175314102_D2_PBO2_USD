d1e0c149a5b67b07ea59130683dc8b623856576d
ACER <ACER@LAPTOP-7IS31PT0>
2018-09-24 16:58:36 +0700

    hmm

---
+++ Pemrograman2/src/ModelUniversitas/ProgramStudi.java
@@ -0,0 +1,28 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package ModelUniversitas;
+
+/**
+ *
+ * @author ACER
+ */
+public class ProgramStudi {
+private String kode;
+private String nama;
+
+
+    public ProgramStudi() {
+    }
+
+    public ProgramStudi(String kode, String nama) {
+        this.kode = kode;
+        this.nama = nama;
+    }
+    
+    
+
+
+}
