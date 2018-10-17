ceabcce03700c1848fbdaa5e194415031250ffa1
admin <admin@21b>
2018-10-17 10:52:54 +0700

    1.0.0

---
+++ Pemrograman2/src/TEST/TestStreaming.java
@@ -0,0 +1,36 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package TEST;
+
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+/**
+ *
+ * @author admin
+ */
+public class TestStreaming {
+
+    public static void main(String[] args) {
+        FileOutputStream fos = null;
+        try {
+            File file = new File("test.txt");
+            String data = "Hello World";
+            fors = new FileOutputStream(file);
+            fos.write(data.getBytes());
+        } catch (FileNotFoundException ex) {
+            Logger.getLogger(TestStreaming.class.getName()).log(Level.SEVERE.getName());
+        }catch{
+        }
+finally{
+    
+    
+        }
+    }
+    
+}
