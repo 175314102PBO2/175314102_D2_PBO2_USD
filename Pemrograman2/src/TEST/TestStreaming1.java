e29ed9d3238be9a418ee63510fd8d3a41a2b2829
admin <admin@27b>
2018-10-24 10:36:59 +0700

    0.0.1

--- Pemrograman2/src/TEST/TestStreaming1.java
+++ Pemrograman2/src/TEST/TestStreaming1.java
@@ -5,22 +5,28 @@
  */
 package TEST;
 
+import java.io.File;
 import java.io.FileInputStream;
+import java.io.FileNotFoundException;
 
 /**
  *
  * @author admin
  */
 public class TestStreaming1 {
-    public static void main(String[] args) {
+
+    public static void main(String[] args) throws FileNotFoundException {
         File file = new File("test.txt");
-        String hasilBaca ="";
+        String hasilBaca = "";
         fis = new FileInputStream(file);
         int dataInt;
-        while(dataInt=fis.read())!=1){
-        hasilBaca = hasilBaca+(char) dataInt;
-    }
+        while (dataInt = fis.read()) {
+             != 1
+        }
+        
+            ){
+        hasilBaca = hasilBaca + (char) dataInt;
+        }
         System.out.println(hasilBaca);
-    }
     }
 }
