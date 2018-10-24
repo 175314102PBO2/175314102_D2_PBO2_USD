d04c52404045555e6308e1cf9c345f4bc6bf2337
admin <admin@27b>
2018-10-24 10:22:42 +0700

    0.0.0.1

--- Pemrograman2/src/TEST/TestStream2.java
+++ Pemrograman2/src/TEST/TestStream2.java
@@ -15,8 +15,8 @@ import java.io.File;
 public class TestStream2 {
     public static void main(String[] args) {
         Pasien.bacaDaftarPasien(new File("daftar.txt"));
-        for (int i = 0; i < Pasien.getDaftarPasien().size(); i++) {
-            System.out.println(Pasien.getDaftarPasien().get(i).toString());
+        for (int i = 0; i < Pasien.getDaftarpasienKlinik().size(); i++) {
+            System.out.println(Pasien.getDaftarpasienKlinik().get(i).toString());
            
         }
     }
