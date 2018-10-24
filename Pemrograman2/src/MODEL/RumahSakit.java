a481ba085b665f332599cfd950c418b742091fc0
admin <admin@27b>
2018-10-24 11:34:48 +0700

    0.0.2

--- Pemrograman2/src/MODEL/RumahSakit.java
+++ Pemrograman2/src/MODEL/RumahSakit.java
@@ -7,7 +7,13 @@ package MODEL;
 
 import static MODEL.Pasien.daftarPasienKlinik;
 import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
 import java.util.ArrayList;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 
 /**
  *
@@ -32,24 +38,68 @@ public class RumahSakit {
                 return daftarPasienKlinik.get(i);
             }
         }
-        //Jika data tidak ditemukan maka akan direturn null
         return null;
     }
 
     public void tambahPasienBaru(Pasien test) {
-
+        daftarPasienKlinik.add(test);
     }
 
     public void simpanDaftarPasien(File file) {
+        FileOutputStream fos = null;
+        try {
+            fos = new FileOutputStream(file, true);
+            for (int i = 0; i < daftarPasienKlinik.size(); i++) {
+                String data = daftarPasienKlinik.get(i).toString();
+                fos.write(data.getBytes());
+            }
+        } catch (FileNotFoundException ex) {
+            Logger.getLogger(Pasien.class.getName()).log(Level.SEVERE, null, ex);
+        } catch (IOException ex) {
+            Logger.getLogger(Pasien.class.getName()).log(Level.SEVERE, null, ex);
+        } finally {
+            try {
+                fos.close();
+            } catch (IOException ex) {
+                Logger.getLogger(Pasien.class.getName()).log(Level.SEVERE, null, ex);
+            }
 
+        }
     }
 
     public void bacaDaftarPasien(File file) {
-
+        FileInputStream fis = null;
+        Pasien tampung = new Pasien();
+        boolean nama = false;
+        boolean alamat = false;
+        String hasil = "";
+        int data;
+        try {
+            fis = new FileInputStream(file);
+            while ((data = fis.read()) > 0) {
+                if ((char) data != '\n') {
+                    if ((char) data != '\t') {
+                        hasil = hasil + daftarPasienKlinik.add(tampung);
+                    } else if (nama = false) {
+                        tampung.setNama(hasil);
+                        nama = true;
+                        hasil = "";
+                    }
+                } else if (alamat = false) {
+                    tampung.setAlamat(hasil);
+                    alamat = true;
+                    hasil = "";
+                }
+            }
+        } catch (FileNotFoundException ex) {
+            Logger.getLogger(Pasien.class.getName()).log(Level.SEVERE, null, ex);
+        } catch (IOException ex) {
+            Logger.getLogger(Pasien.class.getName()).log(Level.SEVERE, null, ex);
+        }
     }
 
     public void simpanObjekRumahSakit(File file) {
-
+    
     }
 
     public void bacaObjekRumahSakit(File file) {
