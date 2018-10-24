48e3ac8f70fc97241ed1150b29355e347a99ccc1
admin <admin@27b>
2018-10-24 10:33:33 +0700

    0.0.1

---
+++ Pemrograman2/src/MODEL/RumahSakit.java
@@ -0,0 +1,32 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package MODEL;
+
+import java.util.ArrayList;
+
+/**
+ *
+ * @author admin
+ */
+public class RumahSakit {
+    private String nama;
+    private String alamat;
+   
+    private ArrayList<Pasien> daftarPasien = new ArrayList<Pasien>();
+    private ArrayList<Klinik> daftarKlinik = new ArrayList<Pasien>();
+
+    public RumahSakit() {
+    }
+
+    public RumahSakit(String nama, String alamat) {
+        this.nama = nama;
+        this.alamat = alamat;
+    }
+    
+    public static Pasien cariPasien(String noRM){
+        
+    }
+}
